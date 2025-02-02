f350a77a1a2e48051e44bd9eac510971cf2c4ae5
Eddie Jeselnik <eddie@jeselnik.xyz>
2025-02-02 20:02:34 +1100

    accidentally committed public/

--- eddie-jeselnik-xyz/public/profile.js
+++
@@ -1,7 +0,0 @@
-// tbd: add medicare #, mygov security question answers
-const profile = {
-  name: "Eddie",
-  profilePic: "pfp.webp",
-  birthday: "July 28, 2000",
-  location: "Melbourne, Australia",
-}
