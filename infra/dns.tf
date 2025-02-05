f39a4918017f549b9f0115b39359fb97448b4e87
Eddie Jeselnik <eddie@jeselnik.xyz>
2025-02-05 22:43:29 +1100

    add mx records

--- infra/dns.tf
+++ infra/dns.tf
@@ -2,12 +2,13 @@ locals {
   cloudflare_zone_id = "8fbf559fa97a8369c33564ddc4bdddf4"
 }
 
+data "cloudflare_zone" "jeselnik" {
+  zone_id = local.cloudflare_zone_id 
+}
+
 resource "porkbun_nameservers" "jeselnik_xyz_nameservers" {
   domain = local.domain
-  nameservers = [
-    "emily.ns.cloudflare.com",
-    "mitch.ns.cloudflare.com"
-  ]
+  nameservers = data.cloudflare_zone.jeselnik.name_servers
 }
 
 # web
@@ -39,8 +40,35 @@ resource "cloudflare_dns_record" "eddie" {
 }
 
 # mail
+resource "cloudflare_dns_record" "mx_one" {
+  zone_id = local.cloudflare_zone_id
+  type = "MX"
+  name = "jeselnik.xyz"
+  content = "mxext1.mailbox.org"
+  priority = 10
+  ttl = 1
+  proxied = false
+}
 
+resource "cloudflare_dns_record" "mx_two" {
+  zone_id = local.cloudflare_zone_id
+  type = "MX"
+  name = "jeselnik.xyz"
+  content = "mxext2.mailbox.org"
+  priority = 10
+  ttl = 1
+  proxied = false
+}
 
+resource "cloudflare_dns_record" "mx_three" {
+  zone_id = local.cloudflare_zone_id
+  type = "MX"
+  name = "jeselnik.xyz"
+  content = "mxext3.mailbox.org"
+  priority = 20
+  ttl = 1
+  proxied = false
+}
 
 # verifications
 resource "cloudflare_dns_record" "aws_acm" {
