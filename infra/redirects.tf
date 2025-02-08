5762f9de0b74cdfc81b8ad24a5b7a471af6485b6
Eddie Jeselnik <eddie@jeselnik.xyz>
2025-02-08 20:38:29 +1100

    redirects tbd

---
+++ infra/redirects.tf
@@ -0,0 +1,27 @@
+locals {
+  cloudflare_account_id = "8940638f1a127e97ec240934e06b73e5"
+}
+
+resource "cloudflare_list" "apex_redirect" {
+  account_id = local.cloudflare_account_id
+  kind       = "hostname"
+  name       = "apex_redirect"
+}
+
+/* https://github.com/cloudflare/terraform-provider-cloudflare/pull/5054
+resource "cloudflare_list_item" "jeselnik_xyz_redirects" {
+  list_id    = cloudflare_list.apex_redirect.id
+  account_id = local.cloudflare_account_id
+  body = [{
+    redirect = {
+      source_url            = "jeselnik.xyz"
+      target_url            = "eddie.jeselnik.xyz"
+      status_code           = 301
+      include_subdomains    = false
+      subpath_matching      = false
+      preserve_query_string = false
+      preserve_path_suffix  = false
+    }
+  }]
+}
+*/
