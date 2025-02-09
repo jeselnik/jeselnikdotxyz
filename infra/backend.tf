37e4ad0ff02ca0765206b2c64dc8f128e8d4ba62
Eddie Jeselnik <eddie@jeselnik.xyz>
2025-02-09 17:05:21 +1100

    cors policy for lambda url

--- infra/backend.tf
+++ infra/backend.tf
@@ -24,14 +24,20 @@ resource "aws_lambda_function" "jeselnik_xyz_backend" {
   package_type  = "Zip"
   handler       = "bootstrap"
   runtime       = "provided.al2023"
-  s3_bucket     = aws_s3_bucket.jeselnik_xyz_lambda.bucket
-  s3_key        = "backend.zip"
-  timeout       = 10
+  /* omg I didn't realise I had to update the code every deploy, that explains it */
+  s3_bucket = aws_s3_bucket.jeselnik_xyz_lambda.bucket
+  s3_key    = "backend.zip"
+  timeout   = 10
 }
 
 resource "aws_lambda_function_url" "visit" {
   function_name      = aws_lambda_function.jeselnik_xyz_backend.function_name
   authorization_type = "NONE"
+
+  cors {
+    allow_methods = ["GET", "POST"]
+    allow_origins = ["https://eddie.${local.domain}"]
+  }
 }
 
 /* keep it simple for now, let's just use a lambda function url
